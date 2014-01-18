535cc76bee74f1647a25af85ff2e5d141cd4cac2
Gabriel Castro <dev@gabrielcastro.ca>
2014-01-18 12:45:17 -0500

    initial commit

---
+++ module/src/main/java/ca/gabrielcastro/xposed/spdy/XposedHook.java
@@ -0,0 +1,16 @@
+package ca.gabrielcastro.xposed.spdy;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class XposedHook implements IXposedHookLoadPackage {
+
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
+
+
+
+    }
+
+
+}
